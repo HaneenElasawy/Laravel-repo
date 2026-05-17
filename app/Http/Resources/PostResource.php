c7e7d3129702559e05fa49a01e1c68a554a8a6b4
Haneenelasawy <Haneenelasawy335@gmail.com>
2026-05-17 15:47:54 +0300

    lab-5 is done

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'content' => $this->content,
+            'image_url' => $this->image ? asset('storage/' . $this->image) : null,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'author' => [
+                'id' => $this->user?->id,
+                'name' => $this->user?->name,
+            ],
+        ];
+    }
+}
